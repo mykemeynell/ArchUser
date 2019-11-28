f8b1f55bfe2d39a923451285b0d598b5889b41c5
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-11-28 10:44:23 +0000

    Update UserEntityInterface.php

--- src/Entity/Contract/UserEntityInterface.php
+++ src/Entity/Contract/UserEntityInterface.php
@@ -31,96 +31,96 @@ interface UserEntityInterface
      *
      * @return string
      */
-    public function getDisplayName(): string;
+    public function getDisplayName();
 
     /**
      * Get the first name of the user.
      *
      * @return string
      */
-    public function getFirstName(): string;
+    public function getFirstName();
 
     /**
      * Get the last name of the user.
      *
      * @return string
      */
-    public function getLastName(): string;
+    public function getLastName();
 
     /**
      * Get the first initial of the user.
      *
      * @return string
      */
-    public function getFirstInitial(): string;
+    public function getFirstInitial();
 
     /**
      * Get the last initial of the user.
      *
      * @return string
      */
-    public function getLastInitial(): string;
+    public function getLastInitial();
 
     /**
      * Get the user's email address.
      *
      * @return string
      */
-    public function getEmail(): string;
+    public function getEmail();
 
     /**
      * Get the user's username.
      *
      * @return null|string
      */
-    public function getUsername(): ?string;
+    public function getUsername();
 
     /**
      * Get the date and time that an email address was verified.
      *
      * @return \Carbon\Carbon|null
      */
-    public function getEmailVerifiedAt(): ?Carbon;
+    public function getEmailVerifiedAt();
 
     /**
      * Test if users email is verified.
      *
      * @return bool
      */
-    public function isEmailVerified(): bool;
+    public function isEmailVerified();
 
     /**
      * Test if the user is root.
      *
      * @return bool
      */
-    public function isRoot(): bool;
+    public function isRoot();
 
     /**
      * Test if the user is active.
      *
      * @return bool
      */
-    public function isActive(): bool;
+    public function isActive();
 
     /**
      * Get the remember token.
      *
      * @return string|null
      */
-    public function getRememberToken(): ?string;
+    public function getRememberToken();
 
     /**
      * Get the forgot password token.
      *
      * @return string|null
      */
-    public function getForgotPasswordToken(): ?string;
+    public function getForgotPasswordToken();
 
     /**
      * Get the internal API token.
      *
      * @return string|null
      */
-    public function getApiToken(): ?string;
+    public function getApiToken();
 }
